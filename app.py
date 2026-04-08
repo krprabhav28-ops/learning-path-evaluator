f1b8ab3b8234c29720e66031d911bfcd0ef6f55d
krprabhav28-ops <krprabhav28@gmail.com>
2026-04-08 21:10:22 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from environment import LearningPathEvaluator
+
+app = FastAPI()
+env = LearningPathEvaluator()
+
+@app.post("/reset")
+def reset():
+    message = env.reset()
+    return{"message": message}
+
+@app.post("/step")
+def step(action: dict):
+    result = env.step()
+    return result
+
+@app.get("/state")
+def state():
+    return env.state()
